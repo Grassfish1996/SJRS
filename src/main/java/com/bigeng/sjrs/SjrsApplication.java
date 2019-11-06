f1ee7ffa552e844d8e5a9f20d4b1e5f26be1602d
HSY <1101347786@qq.com>
2019-11-06 17:18:59 +0800

    初次建立

---
+++ src/main/java/com/bigeng/sjrs/SjrsApplication.java
@@ -0,0 +1,13 @@
+package com.bigeng.sjrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigeng/sjrs/SjrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigeng.sjrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
