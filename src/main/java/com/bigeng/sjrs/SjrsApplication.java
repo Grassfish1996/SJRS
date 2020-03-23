892f9996d9ded3080dc48e7c6739cdba2863d34e
Grassfish1996 <1101347786@qq.com>
2020-03-23 15:29:20 +0800

    添加mybatis plus相关包

--- src/main/java/com/bigeng/sjrs/SjrsApplication.java
+++ src/main/java/com/bigeng/sjrs/SjrsApplication.java
@@ -1,9 +1,11 @@
 package com.bigeng.sjrs;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bigeng.sjrs.mapper")
 public class SjrsApplication {
 
     public static void main(String[] args) {
